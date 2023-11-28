69e406cf1f44fad604279b62e2ba68480843739a
Artem <sfmillingg@gmail.com>
2023-11-28 23:49:28 +0300

    feat: add burger

--- cosmik_redesign/js/main.js
+++ cosmik_redesign/js/main.js
@@ -28,21 +28,30 @@ const burgerBtnSpan = document.querySelectorAll('.header__burger span');
 const burgerNav = document.querySelector('nav') 
 const navItems = document.querySelectorAll('.nav__item');
 const navItemsUl = document.querySelectorAll('.nav__item-ul')
+const navItemsSeminar = document.querySelector('.nav__item-seminar');
+const downUserBtn = document.querySelector('.header__down-login img:nth-child(2)')
+const downMenu = document.querySelector('.header__down-adaptive');
+const downCloseBtn = document.querySelector('.header__down-close');
+const overlay = document.querySelector('.overlay')
 
 console.log(navItemsUl.length)
 
-function activeBurger() {
+function openBurgerMenu() {
     burgerBtn.addEventListener('click', function() {
         if(burgerNav.classList.contains('active-nav')) {
             burgerNav.classList.remove('active-nav');
+            overlay.classList.remove('active-overlay')
+
             removeLine() 
         } else {
             burgerNav.classList.add('active-nav');
+            overlay.classList.add('active-overlay')
             activeLine()
         }
     })
+
 }
-activeBurger()
+openBurgerMenu()
 
 function activeLine() {
     burgerBtnSpan.forEach(item => {
@@ -67,4 +76,22 @@ function activeDrop() {
 
         })
     })
-}activeDrop()
+
+}
+
+activeDrop()
+
+function openUserMenu() {
+    downUserBtn.addEventListener('click', function() {
+        downMenu.classList.add('active-down-adaptive')
+        overlay.classList.add('active-overlay')
+    })
+}
+function closeUserMenu() {
+    downCloseBtn.addEventListener('click', function() {
+        downMenu.classList.remove('active-down-adaptive')
+        overlay.classList.remove('active-overlay')
+    })
+}
+openUserMenu()
+closeUserMenu()
