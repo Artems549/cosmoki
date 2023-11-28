20a432491494a7efe7cb582179ef7460447e867e
Artem <sfmillingg@gmail.com>
2023-11-28 12:54:54 +0300

    feat: add files

---
+++ cosmik_redesign/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#owl-carousel__what-method").owlCarousel({
+        margin: 14,
+        responsive: {
+            320: {
+                items: 1,
+            },
+            750: {
+                items: 4,
+            },
+            1250: {
+                items: 6
+            }
+        }
+    });
+  });
