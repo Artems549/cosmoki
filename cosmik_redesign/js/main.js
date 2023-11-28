197f00ee45eb2962bb6486350a95707f0f515b33
Artem <sfmillingg@gmail.com>
2023-11-28 22:14:07 +0300

    feat: add burger menu

--- cosmik_redesign/js/main.js
+++ cosmik_redesign/js/main.js
@@ -14,3 +14,57 @@ $(document).ready(function(){
         }
     });
   });
+
+
+
+
+
+// Burger menu
+
+
+
+const burgerBtn = document.querySelector('.header__burger');
+const burgerBtnSpan = document.querySelectorAll('.header__burger span');
+const burgerNav = document.querySelector('nav') 
+const navItems = document.querySelectorAll('.nav__item');
+const navItemsUl = document.querySelectorAll('.nav__item-ul')
+
+console.log(navItemsUl.length)
+
+function activeBurger() {
+    burgerBtn.addEventListener('click', function() {
+        if(burgerNav.classList.contains('active-nav')) {
+            burgerNav.classList.remove('active-nav');
+            removeLine() 
+        } else {
+            burgerNav.classList.add('active-nav');
+            activeLine()
+        }
+    })
+}
+activeBurger()
+
+function activeLine() {
+    burgerBtnSpan.forEach(item => {
+        item.classList.add('active')
+    })
+    burgerBtn.classList.add('active-burger');
+}
+
+
+function removeLine() {
+    burgerBtnSpan.forEach(item => {
+        item.classList.remove('active')
+    })
+    burgerBtn.classList.remove('active-burger');
+}
+
+
+function activeDrop() {
+    navItems.forEach((item, i) => {
+        item.addEventListener('click', function() {
+            navItemsUl[i].classList.toggle('active-drop')
+
+        })
+    })
+}activeDrop()
